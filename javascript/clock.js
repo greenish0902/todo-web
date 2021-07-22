09ce49edbbc7d29dfd5ea5ede71aec7db70bc878
greenish0902 <greenish0902@gmail.com>
2021-07-22 23:58:51 +0900

    JS: date

--- javascript/clock.js
+++ javascript/clock.js
@@ -1,10 +1,15 @@
 const clock = document.querySelector("#clock");
+const dateToday = document.querySelector("#date");
 
 function getClock() {
   date = new Date();
+  const year = date.getFullYear().toString();
+  const month = (parseInt(date.getMonth()) + 1).toString().padStart(2, "0");
+  const datenow = date.getDate().toString().padStart(2, "0");
   const day = date.getHours().toString().padStart(2, "0");
   const min = date.getMinutes().toString().padStart(2, "0");
   const sec = date.getSeconds().toString().padStart(2, "0");
+  dateToday.innerText = `It's ${datenow}, ${month}, ${year}.`;
   clock.innerText = `${day}:${min}:${sec}`;
 }
 
